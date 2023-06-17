8d758b27141ec8d49b3c254e303e3fbe3bdcdb08
minhkhuy <khuy.tran@roboworld.io>
2023-06-17 13:34:45 +0700

    feat: init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  return (
+    <Main meta={<Meta title="Messenger" description="Messenger" />}>Hello</Main>
+  );
+};
+
+export default Index;
