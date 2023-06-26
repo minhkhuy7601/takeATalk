276c8844567de6600d09e541d0be6309032c7c6e
minhkhuy <khuy.tran@roboworld.io>
2023-06-26 11:59:45 +0700

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/global.css';
+import 'nprogress/nprogress.css';
 
 import type { AppProps } from 'next/app';
+import { Router } from 'next/router';
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import ActiveStatus from '@/components/ActiveStatus';
@@ -9,14 +13,34 @@ import ToasterContext from '@/context/ToasterContext';
 
 const queryClient = new QueryClient();
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <AuthContext>
-    <ToasterContext />
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-    <ActiveStatus />
-  </AuthContext>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const start = () => {
+      NProgress.start();
+      console.log('start');
+    };
+    const end = () => {
+      NProgress.done();
+      console.log('dones');
+    };
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+  return (
+    <AuthContext>
+      <ToasterContext />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+      <ActiveStatus />
+    </AuthContext>
+  );
+};
 
 export default MyApp;
